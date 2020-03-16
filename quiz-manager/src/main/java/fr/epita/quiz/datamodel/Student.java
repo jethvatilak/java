32d0d7f4c9ce86ab272a850d6d0d8bd591853c95
tilakjethva <tilakjethva@gmail.com>
2020-03-16 11:54:47 +0100

    first commit

---
+++ quiz-manager/src/main/java/fr/epita/quiz/datamodel/Student.java
@@ -0,0 +1,20 @@
+package fr.epita.quiz.datamodel;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "STUDENT")
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "STUDENT_ID")
+	private long id;
+	
+	
+}
